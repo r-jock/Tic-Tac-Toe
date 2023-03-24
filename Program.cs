0eb4cc97ed3a8c5d8119d3921723a8dd8983146b
r-jock <128504586+r-jock@users.noreply.github.com>
2023-03-24 18:45:04 +0100

    Created a VS Project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TicTacToe
+{
+    /* TIC TAC TOE
+     * Created by rene.jock@outlook.de
+     * DATE: 03/24/2023
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### TIC TAC TOE! ###");
+            Console.WriteLine("### by Rene Jock ###");
+        }
+    }
+}
