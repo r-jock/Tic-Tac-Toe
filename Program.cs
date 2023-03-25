1432687c23658e2a374e4fd3d5fb674ba4eefd99
r-jock <128504586+r-jock@users.noreply.github.com>
2023-03-25 12:40:34 +0100

    added the Playfield as Array

--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@
      */
     internal class Program
     {
+        static char player1 = 'X';
+        static char player2 = 'O';
+
         static void Main(string[] args)
         {
-            Console.WriteLine("### TIC TAC TOE! ###");
-            Console.WriteLine("### by Rene Jock ###");
+            char[] field = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            Console.WriteLine(field[0] + " | " + field[1] + " | " + field[2]);
+            Console.WriteLine("---------");
+            Console.WriteLine(field[3] + " | " + field[4] + " | " + field[5]);
+            Console.WriteLine("---------");
+            Console.WriteLine(field[6] + " | " + field[7] + " | " + field[8]);
+
+            Console.ReadKey();
         }
     }
 }
